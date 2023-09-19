615b755104dad5f78f9460a8a463064b600cd11a
Timothy <128176649+Timothy-P@users.noreply.github.com>
2023-09-19 10:32:06 -0500

    Commited :D

---
+++ FC.js
@@ -0,0 +1,26 @@
+const main = document.getElementById("body")
+function InputLoad() {
+    if (localStorage.getItem("InputLocal") != "") {
+        document.InputForm.Numbers.value = localStorage.getItem("InputLocal")
+    };
+};
+function InputBeforeUnload() {
+    localStorage.setItem("InputLocal", document.getElementById("ans"));
+};
+function A() {
+    var B = document.calculator.ans.value;
+    document.calculator.ans.value=eval(document.calculator.ans.value);
+    var C = document.calculator.ans.value;
+    const para = document.createElement("p");
+    const node = document.createTextNode(B + "=" + C);
+    para.appendChild(node);
+    const element = document.getElementById("history");
+    element.appendChild(para);
+};
+function B() {
+    let element = document.getElementById("history");
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+};
+window.addEventListener("beforeunload", InputBeforeUnload())
