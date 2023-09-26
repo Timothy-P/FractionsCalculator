fb46337f52f75a9f965ae4a0f6a53a25e24274ec
Timothy <128176649+Timothy-P@users.noreply.github.com>
2023-09-26 17:40:52 +0000

    Fixing more...

--- FC.js
+++ FC.js
@@ -37,3 +37,20 @@ function parathe() {
 function DEL() {
     calculator.ans.value = calculator.ans.value.slice(0, -1);
 }
+function calculate() {
+    let display = document.getElementById("display");
+    let expression = display.value;
+    let result;
+
+    try {
+        // Convert trigonometric function inputs from degrees to radians
+        expression = expression.replace(/sin\(/g, 'sin(' + Math.PI / 180 + '*');
+        expression = expression.replace(/cos\(/g, 'cos(' + Math.PI / 180 + '*');
+        expression = expression.replace(/tan\(/g, 'tan(' + Math.PI / 180 + '*');
+
+        result = math.evaluate(expression);
+        display.value = result;
+    } catch (error) {
+        display.value = "Error";
+    }
+}
